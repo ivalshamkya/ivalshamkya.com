de236d97a9d78efa4a1b8758d52314770ece79aa
Ival Shamkya <ivalshamkya@gmail.com>
2023-11-16 15:19:27 +0800

    feat(common): edit metadata

--- src/common/constant/metadata.ts
+++ src/common/constant/metadata.ts
@@ -3,7 +3,7 @@ export const METADATA = {
   description:
     "Personal website, portfolio, blog, software engineer roadmap, programming tips of Ival Shamkya",
   keyword:
-    "codebayu, ival shamkya, programming tips, belajar javascript, belajar typescript",
+    "ival shamkya, programming tips, belajar javascript, belajar typescript",
   authors: {
     name: "Ival Shamkya",
     nickname: "Ival",
@@ -13,8 +13,12 @@ export const METADATA = {
     url: "ivalshamkya.com",
     siteName: "Ival Shamkya",
     locale: "id-ID",
+    img: "https://res.cloudinary.com/dlalunvp7/image/upload/v1700074068/profile/Frame_bmcike.jpg"
   },
   exTitle: "| Ival Shamkya",
-  profile:
-    "https://res.cloudinary.com/dlalunvp7/image/upload/ar_1:1,c_fill,g_auto,w_1000/v1697806090/IMG_9165_dug8uz.jpg",
+  profile: {
+    img: "https://res.cloudinary.com/dlalunvp7/image/upload/v1700070073/profile/1903gjv0qfjqwAiqwoik9012if901iasd.jpg",
+    icon: "https://res.cloudinary.com/dlalunvp7/image/upload/v1700071347/profile/IMG_9355_xmuxta.png"
+  }
+    // "https://res.cloudinary.com/dlalunvp7/image/upload/ar_1:1,c_fill,g_auto,w_1000/v1697806090/IMG_9165_dug8uz.jpg",
 };
