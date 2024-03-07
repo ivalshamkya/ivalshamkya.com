b47a8e66d5751f7acfd6af1e5a0d897923252401
ivalshamkya <ivalshamkya@gmail.com>
2024-03-07 10:22:57 +0800

    chore: add new project

--- src/components/Projects/ToolBadge.tsx
+++ src/components/Projects/ToolBadge.tsx
@@ -7,9 +7,9 @@ interface SkillInterfaces {
 
 const ToolBadge: React.FC<SkillInterfaces> = ({ name, icon }) => {
   return (
-    <div className={`w-fit py-1 px-1 md:py-2 md:px-2 flex justify-center items-center ${name ? "gap-2" : ""} bg-white dark:bg-[#252525] border border-slate-200 dark:border-white/10 rounded-lg transition-all linear duration-300`}>
-      <div className="w-3 md:w-4">{icon}</div>
-      <h1 className="whitespace-nowrap text-primary text-[0.6rem] md:text-xs">{name}</h1>
+    <div className={`w-fit py-1 px-1 md:py-2 md:px-2 flex justify-center items-center ${name ? "gap-2" : ""} bg-white dark:bg-[#252525] border border-slate-200 dark:border-white/10 rounded-md transition-all linear duration-300`}>
+      <div className="w-4">{icon}</div>
+      <h1 className="whitespace-nowrap text-primary text-[0.7rem] md:text-xs">{name}</h1>
     </div>
   );
 };
